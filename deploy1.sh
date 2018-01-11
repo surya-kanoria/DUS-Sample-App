4b0bb940a663667994d6b225cd894506e64bc050
surya.kanoria <surya.kanoria@flipkart.com>
2018-01-11 13:03:23 +0530

    updated readme

--- deploy1.sh
+++ deploy1.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 dus-deployer --config DeploymentConfig.json --platform android --react16 true --updateGraphVersion 0.0.0.0 --outputPath output --prodUpdateGraph output/UpdatePatch.json
 node deployUpdatePatch.js
+echo "Deployment done!!!"
